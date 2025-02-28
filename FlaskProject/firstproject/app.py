61fa85f4c9cbbc59a0e4cbadcf0b517567c07a53
BHARAT RAMA NAIK <149295915+EHBHA@users.noreply.github.com>
2025-02-28 10:12:40 +0000

    flask

--- FlaskProject/firstproject/app.py
+++ FlaskProject/firstproject/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template
-
+from flask import Flask, request, render_template, Response, send_from_directory    
+import os
+import uuid
 app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
@@ -21,6 +22,32 @@ def login():
 def filehandle():
     file = request.files['file']
     return file.read().decode()
+
+@app.route("/download_file", methods=['GET', 'POST'])
+def downloadfile():
+    file = request.files['file']
+    response = Response(
+        file,
+        mimetype='text/plain',
+        headers={
+            'Content-Disposition':'attachment; filename=lkb.txt'
+        }
+    )
+    return response
+
+@app.route("/download_file_two", methods=['POST'])
+def download_file_two():
+    file = request.files['file']
+    if not os.path.exists('downloads'):
+        os.mkdir('downloads')
+    filename = f'{uuid.uuid4()}.txt'
+    file_path = os.path.join('downloads', filename)
+    file.save(file_path)
+    return render_template('download.html', filename=filename)
+
+app.route("/download/<filename>")
+def download(filename):
+    return send_from_directory('downloads', filename, download_name='lkb.txt')
 
 
 if __name__=="__main__":
