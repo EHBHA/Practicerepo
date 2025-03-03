9561d16a39b857383fad9b9ef5141e78865cb7a4
BHARAT RAMA NAIK <149295915+EHBHA@users.noreply.github.com>
2025-03-03 04:49:34 +0000

    flask

--- FlaskProject/firstproject/app.py
+++ FlaskProject/firstproject/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, render_template, Response, send_from_directory    
+from flask import Flask, request, render_template, Response, send_from_directory, session
 import os
 import uuid
 app = Flask(__name__, template_folder="templates")
+app.secret_key="12iwuhdwiewiddiwd"
 
 @app.route("/")
 def hello():
@@ -42,13 +43,34 @@ def download_file_two():
         os.mkdir('downloads')
     filename = f'{uuid.uuid4()}.txt'
     file_path = os.path.join('downloads', filename)
+    print(file_path)
     file.save(file_path)
-    return render_template('download.html', filename=filename)
+    return render_template('download.html', filename=file_path)
 
-app.route("/download/<filename>")
+@app.route('/download/<filename>')
 def download(filename):
-    return send_from_directory('downloads', filename, download_name='lkb.txt')
+    return send_from_directory('downloads', filename)
+
+@app.route('/setsession')
+def setsession():
+    session['name']='abcd'
+    session['ho']='qwerty'
+    return render_template('index.html', message="session created")
+
+@app.route('/getsession')
+def getsession():
+    if 'name' in session.keys() and 'ho' in session.keys():
+        name=session['name']
+        ho=session['ho']
+        return render_template('index.html', message=f"name: {name}, ho: {ho}")
+    else:
+        return render_template('index.html', message=f"no session")
+
+@app.route('/clearsession')
+def clearsession():
+    session.clear()
+    return render_template('index.html', message=f"session cleared")
 
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0', port=8008, debug=True)
+    app.run(host='0.0.0.0', port=8018, debug=True)
